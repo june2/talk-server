26efb7c62b478f19ddd6718aa96fb73ebc778dbb
june <kimjy4536@hotmail.com>
2019-07-31 16:10:04 +0900

    update api

--- src/api/room/room.module.ts
+++ src/api/room/room.module.ts
@@ -4,10 +4,11 @@ import { MessageSchema } from './../message/message.schema';
 import { RoomSchema } from './room.schema';
 import { RoomController } from './room.controller';
 import { RoomService } from './room.service';
+import { MessageService } from './../message/message.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'room', schema: RoomSchema }, { name: 'message', schema: MessageSchema }])],
   controllers: [RoomController],
-  providers: [RoomService],
+  providers: [RoomService, MessageService],
 })
 export class RoomModule { }
