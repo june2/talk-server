b9d752c0d30dba6b90f59c6bcac350671f54fab6
june <kimjy4536@hotmail.com>
2019-10-06 23:31:28 +0900

    deploy build

--- dist/common/multer/multer.config.js
+++ dist/common/multer/multer.config.js
@@ -4,8 +4,8 @@ const path_1 = require("path");
 const uuid_1 = require("uuid");
 const common_1 = require("@nestjs/common");
 const AWS = require("aws-sdk");
-const s3Storage = require('multer-sharp-s3');
 const config_service_1 = require("./../config/config.service");
+const s3Storage = require('multer-sharp-s3');
 exports.multerConfig = {
     dest: 'upload',
 };
