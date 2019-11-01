937bce237ba75ef5fb31b3c62555c667dfa647c5
june <kimjy4536@hotmail.com>
2019-11-01 19:49:47 +0900

    update push logic

--- src/common/push/push.service.ts
+++ src/common/push/push.service.ts
@@ -1,54 +1,41 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import Expo from 'expo-server-sdk';
+import * as FCM from 'fcm-node';
 import { User } from './../../api/user/user.interface';
 import { NotificationService } from './../../api/notification/notification.service';
 import { CreateNotificationDto } from './../../api/notification/notification.dto';
 
 @Injectable()
 export class PushService {
-  private readonly expo: Expo;
+  private readonly fcm: FCM;
 
   constructor(private readonly notificationService: NotificationService) {
-    this.expo = new Expo();
+    this.fcm = new FCM();
   }
 
   send(from: User, to: User, body: string, lastMsg: string, roomId: string, type: string) {
-    if (Expo.isExpoPushToken(to.pushToken)) {
-      let messages = [];
-      messages.push({
-        to: to.pushToken,
-        sound: 'default',
+    let message = {
+      to: to.pushToken,
+      // collapse_key: 'your_collapse_key',
+      notification: {
         title: from.name,
-        body: body,
-        data: {
-          type: type,
-          roomId: roomId,
-          userId: from.id,
-          userName: from.name,
-          userImage: (from.images.length > 0) ? from.images[0] : '',
-          msg: lastMsg
-        },
-      })
-
-      let chunks = this.expo.chunkPushNotifications(messages);
-      let tickets = [];
-      (async () => {
-        // Send the chunks to the Expo push notification service. There are
-        // different strategies you could use. A simple one is to send one chunk at a
-        // time, which nicely spreads the load out over time:
-        for (let chunk of chunks) {
-          try {
-            let ticketChunk = await this.expo.sendPushNotificationsAsync(chunk);
-            console.log(ticketChunk);
-            tickets.push(...ticketChunk);
-            // NOTE: If a ticket contains an error code in ticket.details.error, you
-            // must handle it appropriately. The error codes are listed in the Expo
-            this.notificationService.create(new CreateNotificationDto(roomId, to.id, lastMsg));
-          } catch (error) {
-            console.error(error);
-          }
-        }
-      })();
+        body: body
+      },
+      data: {
+        type: type,
+        roomId: roomId,
+        userId: from.id,
+        userName: from.name,
+        userImage: (from.images.length > 0) ? from.images[0] : '',
+        msg: lastMsg
+      },
     }
+    this.fcm.send(message, function (err, response) {
+      if (err) {
+        console.error(`push: ${err}`);
+      } else {
+        console.log(response);
+        this.notificationService.create(new CreateNotificationDto(roomId, to.id, lastMsg));
+      }
+    });
   }
 }
